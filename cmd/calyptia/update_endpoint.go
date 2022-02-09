055d7b9cd848769da352c263c9beec22c44c857d
Phillip Whelan <phil@calyptia.com>
2022-02-09 15:12:27 -0300

    update_endpoint: allow setting frontend and backend port individually by separating them with a colon.

--- cmd/calyptia/update_endpoint.go
+++ cmd/calyptia/update_endpoint.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	cloud "github.com/calyptia/api/types"
 	"github.com/spf13/cobra"
@@ -9,22 +11,46 @@ import (
 
 func newCmdUpdateEndpoint(config *config) *cobra.Command {
 	var protocol string
-	var port uint
-	var pport *uint
 	var portID string
+	var ports string
 
-	if port == 0 {
-		pport = nil
-	} else {
-		pport = &port
-	}
 	cmd := &cobra.Command{
 		Use:   "endpoint",
 		Short: "Update pipeline endpoint",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			var fport, bport uint
+			var fpport, bpport *uint
+
+			if ports != "" {
+				colon := strings.Index(ports, ":")
+				if colon == -1 {
+					port, err := strconv.ParseUint(ports, 10, 16)
+					if err != nil {
+						return fmt.Errorf("unable to parse port number: %w", err)
+					}
+					bport = uint(port)
+					fport = uint(port)
+				} else {
+					port, err := strconv.ParseUint(ports[0:colon], 10, 16)
+					if err != nil {
+						return fmt.Errorf("unable to parse frontend port number: %w", err)
+					}
+					fport = uint(port)
+
+					port, err = strconv.ParseUint(ports[colon+1:len(ports)], 10, 16)
+					if err != nil {
+						return fmt.Errorf("unable to parse frontend port number: %w", err)
+					}
+					bport = uint(port)
+				}
+
+				fpport = &fport
+				bpport = &bport
+			}
+
 			opts := cloud.UpdatePipelinePort{
-				BackendPort:  pport,
-				FrontendPort: pport,
+				BackendPort:  bpport,
+				FrontendPort: fpport,
 				Protocol:     &protocol,
 			}
 			err := config.cloud.UpdatePipelinePort(config.ctx, portID, opts)
@@ -37,7 +63,7 @@ func newCmdUpdateEndpoint(config *config) *cobra.Command {
 
 	fs := cmd.Flags()
 	fs.StringVar(&protocol, "protocol", "", "Endpoint protocol, tcp or tcps")
-	fs.UintVar(&port, "port", 0, "port")
+	fs.StringVar(&ports, "ports", "", "define frontend and backend port, either: [port] or [frotend]:[backend]")
 	fs.StringVar(&portID, "id", "", "Endpoint port ID")
 
 	// _ = cmd.RegisterFlagCompletionFunc("output-format", config.completeOutputFormat)
