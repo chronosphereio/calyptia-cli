3b752affab5ef2e8d9f5a4fbcae50887f8fef983
Jorge Niedbalski <j@calyptia.com>
2022-06-27 15:55:36 +0200

    rename to core_instance.
    I neglected the review by not clarifying that
    aggregator should be core_instance internally and in
    the user facing cli.
    
    Refactoring to ensure we are consistent.
    
    Signed-off-by: Jorge Niedbalski <j@calyptia.com>

---
+++ cmd/calyptia/create_core_instance.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func newCmdCreateCoreInstance(config *config) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "core_instance",
+		Short: "Setup a new core instance on either Kubernetes, Amazon EC2 (TODO), or Google Compute Engine (TODO)",
+	}
+	cmd.AddCommand(newCmdCreateCoreInstanceOnK8s(config, nil))
+	cmd.AddCommand(newCmdCreateCoreInstanceOnAWS(config))
+	cmd.AddCommand(newCmdCreateCoreInstanceOnGCP(config))
+	return cmd
+}
