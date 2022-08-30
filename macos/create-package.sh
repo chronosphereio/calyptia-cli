d077458ee21deddc06527066b2cf2903df366d97
Hiroshi Hatake <hatake@calyptia.com>
2022-08-30 17:08:17 +0900

    Suppress shellcheck linting errors
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- macos/create-package.sh
+++ macos/create-package.sh
@@ -2,18 +2,18 @@
 set -x
 
 OS=$1
-ROOT=$(dirname $2)
+ROOT=$(dirname "$2")
 VERSION=$3
 ARCH=$4
 
-if [ $OS = "darwin" ]; then
-    mkdir -p macos/${ARCH}
+if [ "${OS}" = "darwin" ]; then
+    mkdir -p "macos/${ARCH}"
     pkgbuild --quiet \
-             --root $ROOT \
+             --root "${ROOT}" \
              --identifier "com.calyptia.cli" \
-             --version $VERSION \
+             --version "${VERSION}" \
              --install-location "/usr/local/bin" "macos/${ARCH}/calyptia-cli.pkg"
 
-    productbuild --quiet --package macos/${ARCH}/calyptia-cli.pkg calyptia-cli-v${VERSION}-${ARCH}.pkg
-    rm -f macos/${ARCH}/calyptia-cli.pkg
+    productbuild --quiet --package "macos/${ARCH}/calyptia-cli.pkg" "calyptia-cli-v${VERSION}-${ARCH}.pkg"
+    rm -f "macos/${ARCH}/calyptia-cli.pkg"
 fi
