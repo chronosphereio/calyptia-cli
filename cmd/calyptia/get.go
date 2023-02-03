37067daaa939f9ef9d5ceb61196bfcc9767b6ad4
Phillip Whelan <phil@calyptia.com>
2023-02-03 18:33:47 -0300

    cluster_objects: add preliminary support for cluster objects.
    The new commands added are:
    
    - get cluster_objects: used to get all the cluster objects
    - update pipeline_cluster_objects
        [--pipeline PIPELINE_KEY]
        [--cluster_object CLUSTER_OBJECT_KEY]: used to enable
          cluster logging on a namespace.
    
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- cmd/calyptia/get.go
+++ cmd/calyptia/get.go
@@ -21,6 +21,7 @@ func newCmdGet(config *config) *cobra.Command {
 		newCmdGetPipelineSecrets(config),
 		newCmdGetPipelineFiles(config),
 		newCmdGetPipelineFile(config),
+		newCmdGetClusterObjects(config),
 		newCmdGetResourceProfiles(config),
 		newCmdGetEnvironment(config),
 		newCmdGetTraceSessions(config),
