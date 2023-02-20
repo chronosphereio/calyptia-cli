71f7fabc86da437b97ec0afa018aa741edbc1b24
Gabriel Bussolo <gabriel@calyptia.com>
2023-02-20 20:28:47 +0300

    Create a package to save data locally (#300)
    Signed-off-by: Gabriel Bussolo <gabriel@calyptia.com>

--- cmd/calyptia/auth.go
+++ cmd/calyptia/auth.go
@@ -6,100 +6,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/fs"
-	"os"
-	"path/filepath"
-
-	"github.com/zalando/go-keyring"
 )
 
 const serviceName = "cloud.calyptia.com"
+const backUpFolder = ".calyptia"
 
-var (
-	errTokenNotFound = errors.New("token not found")
-	errInvalidToken  = errors.New("invalid token")
-)
-
-func saveToken(token string) error {
-	err := keyring.Set(serviceName, "project_token", token)
-	if err == nil {
-		return nil
-	}
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return fmt.Errorf("could not get user home dir: %w", err)
-	}
-
-	fileName := filepath.Join(home, ".calyptia", "project_token")
-	if _, err := os.Stat(fileName); os.IsNotExist(err) {
-		dir := filepath.Dir(fileName)
-		err = os.MkdirAll(dir, fs.ModePerm)
-		if err != nil {
-			return fmt.Errorf("could not create directory %q: %w", dir, err)
-		}
-	}
-
-	err = os.WriteFile(fileName, []byte(token), fs.ModePerm)
-	if err != nil {
-		return fmt.Errorf("could not store file %q: %w", fileName, err)
-	}
-
-	return nil
-}
-
-func savedToken() (string, error) {
-	token, err := keyring.Get(serviceName, "project_token")
-	if err == keyring.ErrNotFound {
-		return "", errTokenNotFound
-	}
-
-	if err == nil {
-		return token, nil
-	}
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return "", fmt.Errorf("could not get user home dir: %w", err)
-	}
-
-	b, err := readFile(filepath.Join(home, ".calyptia", "project_token"))
-	if errors.Is(err, fs.ErrNotExist) {
-		return "", errTokenNotFound
-	}
-
-	if err != nil {
-		return "", err
-	}
-
-	token = string(b)
-
-	return token, nil
-}
-
-func deleteSavedToken() error {
-	err := keyring.Delete(serviceName, "project_token")
-	if err == nil {
-		return nil
-	}
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return err
-	}
-
-	fileName := filepath.Join(home, ".calyptia", "project_token")
-	if _, err := os.Stat(fileName); os.IsNotExist(err) {
-		return nil
-	}
-
-	err = os.Remove(fileName)
-	if err != nil {
-		return fmt.Errorf("could not delete default project token: %w", err)
-	}
-
-	return nil
-}
+var errInvalidToken = errors.New("invalid token")
 
 type projectTokenPayload struct {
 	ProjectID string // no json tag
--- cmd/calyptia/config_url.go
+++ cmd/calyptia/config_url.go
@@ -3,16 +3,14 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io/fs"
 	"net/url"
-	"os"
-	"path/filepath"
 
 	"github.com/spf13/cobra"
-	"github.com/zalando/go-keyring"
 )
 
 var errURLNotFound = errors.New("url not found")
+
+const KeyBaseURL = "base_url"
 
 func newCmdConfigSetURL(config *config) *cobra.Command {
 	return &cobra.Command{
@@ -29,7 +27,7 @@ func newCmdConfigSetURL(config *config) *cobra.Command {
 				return fmt.Errorf("invalid cloud url scheme %q", cloudURL.Scheme)
 			}
 
-			err = saveURL(cloudURL.String())
+			err = config.localData.Save(KeyBaseURL, cloudURL.String())
 			if err != nil {
 				return err
 			}
@@ -57,7 +55,7 @@ func newCmdConfigUnsetURL(config *config) *cobra.Command {
 		Use:   "unset_url",
 		Short: "Unset the current configured default cloud URL",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			err := deleteSavedURL()
+			err := config.localData.Delete(KeyBaseURL)
 			if err != nil {
 				return err
 			}
@@ -65,85 +63,4 @@ func newCmdConfigUnsetURL(config *config) *cobra.Command {
 			return nil
 		},
 	}
-}
-
-func saveURL(url string) error {
-	err := keyring.Set(serviceName, "base_url", url)
-	if err == nil {
-		return nil
-	}
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return fmt.Errorf("could not get user home dir: %w", err)
-	}
-
-	fileName := filepath.Join(home, ".calyptia", "base_url")
-	if _, err := os.Stat(fileName); os.IsNotExist(err) {
-		dir := filepath.Dir(fileName)
-		err = os.MkdirAll(dir, fs.ModePerm)
-		if err != nil {
-			return fmt.Errorf("could not create directory %q: %w", dir, err)
-		}
-	}
-
-	err = os.WriteFile(fileName, []byte(url), fs.ModePerm)
-	if err != nil {
-		return fmt.Errorf("could not store file %q: %w", fileName, err)
-	}
-
-	return nil
-}
-
-func deleteSavedURL() error {
-	err := keyring.Delete(serviceName, "base_url")
-	if err == nil {
-		return nil
-	}
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return err
-	}
-
-	fileName := filepath.Join(home, ".calyptia", "base_url")
-	if _, err := os.Stat(fileName); os.IsNotExist(err) {
-		return nil
-	}
-
-	err = os.Remove(fileName)
-	if err != nil {
-		return fmt.Errorf("could not delete default project url: %w", err)
-	}
-
-	return nil
-}
-
-func savedURL() (string, error) {
-	url, err := keyring.Get(serviceName, "base_url")
-	if err == keyring.ErrNotFound {
-		return "", errURLNotFound
-	}
-
-	if err == nil {
-		return url, nil
-	}
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return "", fmt.Errorf("could not get user home dir: %w", err)
-	}
-
-	b, err := readFile(filepath.Join(home, ".calyptia", "base_url"))
-	if errors.Is(err, fs.ErrNotExist) {
-		return "", errURLNotFound
-	}
-
-	if err != nil {
-		return "", err
-	}
-
-	url = string(b)
-
-	return url, nil
 }
