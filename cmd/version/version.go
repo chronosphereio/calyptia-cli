3c5470dafe1d21d9313f79171257c1161effd703
jasmingacic <jasmin@calyptia.com>
2023-02-23 18:18:23 +0000

    Added calyptia version subcommand
    Signed-off-by: jasmingacic <jasmin@calyptia.com>

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -1,6 +1,26 @@
 package version
 
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
 var (
 	DefaultCloudURLStr = "https://cloud-api.calyptia.com"
 	Version            = "dev" // To be injected at build time:  -ldflags="-X 'github.com/calyptia/cli/cmd/version.Version=xxx'"
 )
+
+func NewVersionCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:           "version",
+		Short:         "v",
+		SilenceErrors: true,
+		SilenceUsage:  true,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Version", Version)
+		},
+	}
+
+	return cmd
+}
