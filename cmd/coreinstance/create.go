c75383f2c35ea017d552712e469b5c719804389b
Gabriel Bussolo <gabriel@calyptia.com>
2023-07-07 12:02:40 +0300

    Set on the parent a check for token on creation
    Signed-off-by: Gabriel Bussolo <gabriel@calyptia.com>

--- cmd/coreinstance/create.go
+++ cmd/coreinstance/create.go
@@ -1,6 +1,7 @@
 package coreinstance
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 
 	cfg "github.com/calyptia/cli/config"
@@ -8,12 +9,22 @@ import (
 
 func NewCmdCreateCoreInstance(config *cfg.Config) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "core_instance",
-		Short: "Setup a new core instance on either Kubernetes, Amazon EC2, or Google Compute Engine",
+		Use:               "core_instance",
+		Short:             "Setup a new core instance on either Kubernetes, Amazon EC2, or Google Compute Engine",
+		PersistentPreRunE: checkForProjectToken(config),
 	}
 	cmd.AddCommand(newCmdCreateCoreInstanceOnK8s(config, nil))
 	cmd.AddCommand(newCmdCreateCoreInstanceOnAWS(config, nil, nil))
 	cmd.AddCommand(newCmdCreateCoreInstanceOnGCP(config, nil))
 	cmd.AddCommand(newCmdCreateCoreInstanceOperator(config, nil))
 	return cmd
+}
+
+func checkForProjectToken(config *cfg.Config) func(cmd *cobra.Command, args []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		if config.ProjectToken == "" {
+			return fmt.Errorf("project token is required to realize this action.\nPlease set it with `calyptia config set_token <token>`")
+		}
+		return nil
+	}
 }
