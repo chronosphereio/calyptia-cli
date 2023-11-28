39872c44789b2a93190fd2c65bd5f73161aae1c8
jasmingacic <jasmin@calyptia.com>
2023-11-28 12:03:44 +0000

    shebang

--- smoke-test/test.sh
+++ smoke-test/test.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo "getting latest operator image version"
 latest=$(curl -sf https://api.github.com/repos/calyptia/core-operator-releases/releases | jq -r .[0].tag_name)
 
@@ -5,12 +6,12 @@ echo "getting version before latest operator image version"
 beforelatest=$(curl -sf https://api.github.com/repos/calyptia/core-operator-releases/releases | jq -r .[1].tag_name)
 echo "test install operator into default namespace"
 ./calyptia install operator 
-./calyptia update operator --version $beforelatest --verbose
-./calyptia update operator --version $latest --verbose
+./calyptia update operator --version "$beforelatest" --verbose
+./calyptia update operator --version "$latest" --verbose
 ./calyptia uninstall operator 
 
 echo "test install operator into test namespace"
 ./calyptia install operator --kube-namespace test
-./calyptia update operator --version $beforelatest --kube-namespace test--verbose
+./calyptia update operator --version "$beforelatest" --kube-namespace test--verbose
 
 
