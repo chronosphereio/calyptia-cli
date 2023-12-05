5112d29ec59dab12a6c0b85a18a3b60fbf0f8f3e
jasmingacic <jasmin@calyptia.com>
2023-12-05 14:26:01 +0000

    revert version

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -6,7 +6,7 @@ import (
 
 var (
 	DefaultCloudURLStr = "https://cloud-api.calyptia.com"
-	Version            = "1.0.0" // To be injected at build time:  -ldflags="-X 'github.com/calyptia/cli/cmd/version.Version=xxx'"
+	Version            = "dev" // To be injected at build time:  -ldflags="-X 'github.com/calyptia/cli/cmd/version.Version=xxx'"
 )
 
 func NewVersionCommand() *cobra.Command {
