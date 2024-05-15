d299b7db20e06ff227993608410aacc7ca5d0330
calyptia-ci <98808777+calyptia-ci@users.noreply.github.com>
2024-05-15 09:39:59 +0100

    ci: update to latest Core Operator version (#843)
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: jasmingacic <8327751+jasmingacic@users.noreply.github.com>

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -16,15 +16,15 @@ const (
 
 	DefaultCoreOperatorDockerImage = "ghcr.io/calyptia/core-operator"
 	// DefaultCoreOperatorDockerImageTag not manually modified, CI should switch this version on every new release.
-	DefaultCoreOperatorDockerImageTag = "v2.11.0"
+	DefaultCoreOperatorDockerImageTag = "v2.12.0"
 
 	DefaultCoreOperatorToCloudDockerImage = "ghcr.io/calyptia/core-operator/sync-to-cloud"
 	// DefaultCoreOperatorToCloudDockerImageTag not manually modified, CI should switch this version on every new release.
-	DefaultCoreOperatorToCloudDockerImageTag = "v2.11.0"
+	DefaultCoreOperatorToCloudDockerImageTag = "v2.12.0"
 
 	DefaultCoreOperatorFromCloudDockerImage = "ghcr.io/calyptia/core-operator/sync-from-cloud"
 	// DefaultCoreOperatorFromCloudDockerImageTag not manually modified, CI should switch this version on every new release.
-	DefaultCoreOperatorFromCloudDockerImageTag = "v2.11.0"
+	DefaultCoreOperatorFromCloudDockerImageTag = "v2.12.0"
 )
 
 type RecordCell struct {
